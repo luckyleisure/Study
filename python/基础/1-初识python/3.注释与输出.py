16e2a4eaa8fe87f4c64b9e484b46aad7d5a7308d
luckyleisure <54757349+luckyleisure@users.noreply.github.com>
2022-10-16 18:37:30 +0800

    Create 3.注释与输出.py

---
+++ python/基础/1-初识python/3.注释与输出.py
@@ -0,0 +1,42 @@
+"注释的作用：起到解释说明，增强可读性"
+# 快速注释多行： 先选中代码，ctrl+/
+
+# 下面的语句是打印hello world
+# print('hello world')
+
+# 多行注释  ---- 一对三引号
+'''
+这是一对三引号
+三引号是引号键按三下
+'''
+""" 
+床前明月光
+疑是地上霜
+"""
+
+"print() 打印"
+# print('hello')
+# print('hello','world')
+# print('hello',end='\n')  # 参数end默认是换行符，\n换行符
+# print('hello',end='☆')
+# print('hello',end='')
+
+# print('hello',end='world')
+# print('hello',end='')
+
+# 打印皮卡丘
+print('''
+    へ　　　　　／|
+　　/＼7　　　 ∠＿/
+　 /　│　　 ／　／
+　│　Z ＿,＜　／　　 /`ヽ
+　│　　　　　ヽ　　 /　　〉
+　 Y　　　　　`　 /　　/
+　ｲ●　､　●　　⊂⊃〈　　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿
+  ''')
