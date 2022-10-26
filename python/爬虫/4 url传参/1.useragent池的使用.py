5431491cb2678329163351ff4999eba7ead55b56
luckyleisure <54757349+luckyleisure@users.noreply.github.com>
2022-10-26 22:12:52 +0800

    Update and rename 1.useragent池的使用 to 1.useragent池的使用.py

--- python/爬虫/4 url传参/1.useragent池的使用.py
+++ python/爬虫/4 url传参/1.useragent池的使用.py
@@ -1,10 +1,11 @@
 """
 什么是user-agent池?
-包含了多个用户代理的容器(列表，元组，集合..)
+    包含了多个用户代理的容器(列表，元组，集合..)
 有什么用?
-可以达到反反爬的效果1
+    可以达到反反爬的效果1
 如果使用同一个ua 去进行网页的访问  可能会出现被反扒的情况
 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appme/105.0.0.0 Safari/537.36"
+
 zhangsan_requests_count=100
 if zhangsan_requests_count>=100:
     反扒!
@@ -20,17 +21,16 @@ if zhangsan_requests_ua=="Mozilla/5.0 (Windows NT 10.0; Win64; x64) Appme/106.0.
 需要先拿一个正确的ua  然后通过理性的方法重新声明新的ua，然后放到列表
 循环遍历使用 不同ua
 """
-# ua_list=[
-#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
-#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-# ]
-# for ua in ua_list:
-#     headers={
-#         "user-agent":ua
-#     }
-#     print(headers)
-
+ua_list = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+]
+for ua in ua_list:
+    headers = {
+        "user-agent": ua
+    }
+    print(headers)
 
 """
 第二种使用随机的方法 获取ua  
@@ -45,22 +45,22 @@ ua_list = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
 ]
 # 1随机索引  由于列表只有3条数据  索引0开始 2结束 就写入0 2
-# index=random.randint(0,2)
-# print(index)
-# headers={
-#     "user-agent":ua_list[index]
-# }
-# print(headers)
+index = random.randint(0, 2)
+print(index)
+headers = {
+    "user-agent": ua_list[index]
+}
+print(headers)
 # 2直接随机值 通过random.choices随机获取ualist里面的ua   返回形式是一个包含了ua的列表
-# ua=random.choices(ua_list)
-#
-# ua2=random.choice(ua_list)
-# print(ua)
-# headers={
-#     "user-agent":ua2
-# }
-#
-# print(headers)
+ua = random.choices(ua_list)
+
+ua2 = random.choice(ua_list)
+print(ua)
+headers = {
+    "user-agent": ua2
+}
+
+print(headers)
 
 # 通过第三方模块实现生成ua的功能
 # fake-useragent  默认是属于联网
@@ -73,6 +73,7 @@ from fake_useragent import FakeUserAgent
 # 通过FakeUserAgent类的random方法 获取一个随机请求的ua
 useragent = FakeUserAgent().random
 print(useragent)
+
 """
 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17
 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36
@@ -81,7 +82,7 @@ Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .N
 需要进行配置  
 """
 
-# 第一部运行下面的代码 查看到自己的路径
+# # 第一部运行下面的代码 查看到自己的路径
 import os
 import tempfile
 
