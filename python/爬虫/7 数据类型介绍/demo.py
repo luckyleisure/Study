e3c4259a7752a92754195c7b07327f1da3d7bd14
luckyleisure <54757349+luckyleisure@users.noreply.github.com>
2022-11-06 15:23:14 +0800

    Update demo.py

--- python/爬虫/7 数据类型介绍/demo.py
+++ python/爬虫/7 数据类型介绍/demo.py
@@ -48,14 +48,41 @@ data = {
     "sort_new": "hot_desc",
     "city_name": "南京",
     "limit": "30",
-    "page": "5"
+    "page": "0"
 }
 response = requests.post(url=urls, headers=headers, data=data)
 datastr = response.content.decode()
-print(type(datastr))
+# print(type(datastr))
 data = json.loads(datastr)
-print(type(data), data)
+# print(type(data), data)
 strdata = json.dumps(data, ensure_ascii=False, indent="\t")
 print(type(strdata), strdata)
 with open("车.json", "w", encoding="utf-8") as file1:
     file1.write(strdata)
+"""------------------------------二级目录---------------------------------------"""
+
+import requests
+import re
+import json
+
+urls = "https://www.dongchedi.com/motor/pc/car/brand/select_series_v2"
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    "cookie": "ttwid=1%7CpjAlR_h5bH0KLALBYDM--RXYH8vd9SLcynPIQMZbCOg%7C1667483837%7Ca193d8a75916923f802f20cba7986008f3039c4a8e526a48e37eae42cc257584; tt_webid=7161788507171276296; tt_web_version=new; MONITOR_WEB_ID=ed4931de-1628-43b5-b411-9e79644579b4; s_v_web_id=verify_la14tpn9_DIb8ihUI_XsGU_4NDY_ApCh_eo6AWLItrok9; _ga=GA1.2.904659687.1667483842; _gid=GA1.2.1551109384.1667483842; city_name=%E5%8D%97%E4%BA%AC; is_dev=false; is_boe=false; Hm_lvt_3e79ab9e4da287b5752d8048743b95e6=1667483839,1667568269; Hm_lpvt_3e79ab9e4da287b5752d8048743b95e6=1667568269; _gat_gtag_UA_138671306_1=1"
+}
+data = {
+    "country": "0",
+    "sort_new": "hot_desc",
+    "city_name": "南京",
+    "limit": "30",
+    "page": "0"
+}
+response = requests.post(url=urls, headers=headers, data=data)
+datastr = response.content.decode()
+print(datastr)
+id = re.findall('"concern_id":(.*?),"', datastr)
+print(id)
+second_half_url = "https://www.dongchedi.com/auto/series/"
+for i in id:
+    second_url = second_half_url + i
+    print(second_url)
